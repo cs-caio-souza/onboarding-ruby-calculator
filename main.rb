763ceb4da72ca7cc69f70024230feb60959722d9
Unknown <caio.souza@concrete.com.br>
2016-10-14 14:49:13 -0300

    Include sub operation

--- main.rb
+++ main.rb
@@ -3,11 +3,18 @@ def add
   puts "The sum is... #{gets.to_i + gets.to_i}"
 end
 
+def sub
+  puts "Which numbers would you like to subtract?"
+  puts "The subtraction is... #{gets.to_i - gets.to_i}"
+end
+
 loop do
-  puts "Would you like to [a]dd or [q]uit?"
+  puts "Would you like to [a]dd, [s]ubtract or [q]uit?"
   case gets.chomp
   when "a"
     add
+  when "s"
+    sub
   when "q"
     break
   end
